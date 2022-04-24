361868397c42c2eceaf09c5116f78bb622dc211a
wiktord2000 <wiktord2000@wp.pl>
2022-04-24 21:58:37 +0200

    Add models

---
+++ Lab3/group_finder/src/Models/SingleAd.js
@@ -0,0 +1,12 @@
+
+export class SingleAd{
+
+    constructor(id, name, email, description, tags, courses) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.description = description;
+      this.tags = tags;
+      this.courses = courses;
+    }
+}
