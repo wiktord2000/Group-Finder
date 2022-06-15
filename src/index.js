2939e8f2bde3265f071032136a25493343a746c6
wiktord2000 <wiktord2000@wp.pl>
2022-06-15 13:49:18 +0200

    Fix error with antd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import { BrowserRouter} from 'react-router-dom';
 import { LoginProvider } from './Providers/LoginProvider';
 
