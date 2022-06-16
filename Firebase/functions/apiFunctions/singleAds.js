812edc7dc59323beeb7fbc8dbda8e4007d12ed1b
wiktord2000 <wiktord2000@wp.pl>
2022-06-16 19:33:25 +0200

    Finish CRUD operations for users collection

--- Firebase/functions/apiFunctions/singleAds.js
+++ Firebase/functions/apiFunctions/singleAds.js
@@ -4,7 +4,11 @@ const admin = require('firebase-admin');
 
 
 
+
 const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
 
 
 const functions = require("firebase-functions");
--- Firebase/functions/apiFunctions/users.js
+++ Firebase/functions/apiFunctions/users.js
@@ -1,12 +1,14 @@
-const express = require('express');         // For API methods
+const express = require('express');                 // For API methods
 const cors = require('cors');
 const admin = require('firebase-admin');
-
-const app = express();
-
-
 const functions = require("firebase-functions");
 
+const app = express();
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+
+// Get all users
 app.get('/', async (req, res) => {
     
     const snapshot = await admin.firestore().collection('users').get();
@@ -23,6 +25,24 @@ app.get('/', async (req, res) => {
     res.status(200).send(JSON.stringify(users));
 });
 
+
+// Get specific user
+app.get('/:id', async (req, res) => {
+     
+   await admin.firestore().collection('users').doc(req.params.id).get().then(
+        doc => {
+            if(doc.exists){ 
+                res.status(200).send(JSON.stringify({id: doc.id, ...doc.data()}));
+            }
+            else{
+                res.status(404).send("Error: Record not found");
+            }
+        }
+    );    
+});
+
+
+// Add specific user
 app.post('/', async (req, res) => {
 
     // Read user data from body
@@ -34,4 +54,27 @@ app.post('/', async (req, res) => {
 });
 
 
-exports.user = functions.region('europe-central2').https.onRequest(app);
+// Update specific user
+app.put('/:id', async (req, res) => {
+
+    // Get new user data from body
+    const updatedUser = req.body;
+    // Find and update user in collection
+    await admin.firestore().collection('users').doc(req.params.id).update(updatedUser.data);
+    // Send response
+    res.status(200).send();
+});
+
+
+// Delete specific user
+app.delete('/:id', async (req, res) => {
+
+    // Find and delete user in collection
+    await admin.firestore().collection('users').doc(req.params.id).delete();
+    // Send response
+    res.status(200).send();
+});
+
+
+// Exports functions
+exports.userAPI = functions.region('europe-central2').https.onRequest(app);
